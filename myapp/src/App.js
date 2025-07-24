6ab7a78dd92348a4bd45a51ecf8e2898aee61926
sudarshan sangle <darshsangle9456@gmail.com>
2025-07-24 13:45:38 +0530

    Fix: added myapp as normal folder, removed submodule

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navabr from './components/Navabr';
+
+function App() {
+  return (
+    <div className="">
+      <Navabr/>
+     <h1 className='font-bold'>ok</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/Navabr.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Navabr() {
+  return (
+    <div className=' h-[100px] w-[100%]flex justify-between '>
+         <div className=''>
+        
+      
+        </div>
+        <div className=''>
+        
+      
+        </div>
+        <div className=''>
+        
+      
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navabr
